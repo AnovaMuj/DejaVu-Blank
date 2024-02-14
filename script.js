60ff30218313e91276542ba3f79803f049c00ed5
AnovaMuj <146035797+AnovaMuj@users.noreply.github.com>
2024-02-14 20:33:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const downloadFile = (file) => {
+  const element = document.createElement("a");
+  element.setAttribute("href", file);
+  element.setAttribute("download", file);
+
+  element.style.display = "none";
+
+  document.body.appendChild(element);
+
+  element.click();
+  document.body.removeChild(element);
+};
+
+downloadFile("Final.mp3");
+downloadFile("protected.zip");
+
+var myElement = document.body;
+myElement.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+});
